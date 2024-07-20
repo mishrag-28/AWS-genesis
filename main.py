2b555081057e9bf11a1e5fb0aef3b19ae3ccb715
Gagan Kumar Mishra <147093460+mishrag-28@users.noreply.github.com>
2024-07-20 16:44:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 20:45:51 2024
+
+@author: gm205
+"""
+from fastapi import FastAPI
+# from starlette.middleware.cors import CORSMiddleware
+#from Text import Text
+from modelfile import NERModel
+import uvicorn
+from pydantic import BaseModel
+# Load the NER module
+ner = NERModel()
+
+# User's input text is sent as a request body. The structure of this body can be defined by extending Pydantic's BaseModel
+class Text(BaseModel):
+    text: str
+# The model above declares a JSON object (or Python dict) like:
+# {
+#     "text": "user's input text"
+# }
+
+# Initialize the FastAPI application
+app = FastAPI()
+
+# Allow Cross-Origin Resource Sharing (CORS) requests to from any host so that the JavaScript in the extension can communicate with the server
+# app.add_middleware(
+#     CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+# )
+
+# API root
+@app.get("/")
+def get_root():
+    return "This is the RESTful API for your NER application"
+
+# POST endpoint with path '/predict'
+@app.post("/predict")
+async def predict_entities(text: Text):
+    entities = ner.predict_entities(text.text)
+
+    return {
+        "original": text.text,
+        "entities": entities
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8080)
+    # uvicorn.run(app, host='0.0.0.0', port=6000)
+#uvicorn main:app --reload
+
+
+        # "default_popup": "popup.html"
---
+++ modelfile.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 21:00:33 2024
+
+@author: gm205
+"""
+
+import torch
+from transformers import DistilBertForTokenClassification, DistilBertTokenizer
+
+class NERModel:
+    def __init__(self):
+        if(torch.cuda.is_available()):
+            self.device = torch.device("cuda:0")
+        else:
+            self.device = torch.device("cpu:0")
+
+        # DistilBert Tokenizer & Model for NER
+        print("Loading DistilBert Tokenizer & Model for NER.")
+        ner_model_name = "Raj-sharma01/results"
+        # ner_model_name = "Gkumi/fresh-model-uncased"
+        self.tokenizer = DistilBertTokenizer.from_pretrained(ner_model_name)
+        self.model = DistilBertForTokenClassification.from_pretrained(ner_model_name).to(self.device)
+
+    def predict_entities(self, text):
+        inputs = self.tokenizer(text, truncation=True, padding=True, return_tensors="pt").to(self.device)
+        outputs = self.model(**inputs)
+        predictions = torch.argmax(outputs.logits, dim=2)
+        
+        # Convert the input_ids back to words
+        input_words = [self.tokenizer.decode([id]) for id in inputs["input_ids"].tolist()[0]]
+        
+        # Convert the predictions to entity labels
+        predicted_labels = [self.model.config.id2label[id] for id in predictions[0].tolist()]
+        
+        # Pair the words with their predicted entities
+        entities = [(word, label) for word, label in zip(input_words, predicted_labels)]
+        
+        # Initialize an empty list for formatted output
+        formatted_output = []
+        
+        # Iterate over words and their predicted labels
+        for word, label in entities:
+            if word in ["[CLS]", "[SEP]"]:
+                # Ignore special tokens
+                continue
+            elif word.startswith("##"):
+                # This is a word piece, remove '##' and append it to the last word
+                formatted_output[-1]["word"] += word[2:]
+            else:
+                # This is a new word, add it to the formatted output
+                # Remove 'B-' and 'I-' prefixes from the label
+                label = label.replace('B-', '').replace('I-', '')
+                if formatted_output and formatted_output[-1]["entity"] == label:
+                    # If the current entity is the same as the previous one, append the word to the previous word
+                    formatted_output[-1]["word"] += " " + word
+                else:
+                    # Otherwise, add a new entry to the formatted output
+                    formatted_output.append({"word": word, "entity": label if label != 'O' else None})
+    # for printing entire text use this below line of code
+     #result = ' '.join([f"{item['word']} ({item['entity']})" if item['entity'] else item['word'] for item in formatted_output])       
+        result = [f"{item['word']} ({item['entity']})" for item in formatted_output if item['entity']]
+        return result
+
+
+
+
+
+
+
+
+
