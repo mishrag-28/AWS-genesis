a9e616152c076f7cbca47a02867eb0e7e2f25be2
Gagan Kumar Mishra <147093460+mishrag-28@users.noreply.github.com>
2024-08-12 00:07:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,9 @@ async def predict_entities(text: Text):
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', 
                 port=8080, 
-                ssl_keyfile="/etc/letsencrypt/live/ec2instance.ethix4ai.com/privkey.pem", 
-                ssl_certfile="/etc/letsencrypt/live/ec2instance.ethix4ai.com/fullchain.pem")
+                ssl_keyfile="/etc/letsencrypt/archive/ec2instance.ethix4ai.com/privkey1.pem", 
+                ssl_certfile="/etc/letsencrypt/archive/ec2instance.ethix4ai.com/fullchain1.pem")
+
 
     # uvicorn.run(app, host='0.0.0.0', port=8080)
     # # uvicorn.run(app, host='0.0.0.0', port=6000)
